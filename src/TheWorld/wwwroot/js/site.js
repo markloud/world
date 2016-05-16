a0b7069e9d8cbb769db66054b699fa4ad111cba7
Mark Anthony Chua <MarkAnthony.Chua@infor.com>
2016-05-16 23:37:41 +0800

    javascript: bower for JS (jquery, underscore), toggleClass(), transition css, Alt + Shift + W = sorround with <div>, (function () {} ) (); = execute this anonymous class to avoid global variable clutter, Wrapped set = combined jquery objects

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+// site.js
+(function () {
+    //var ele = $("#username");
+    //ele.text("MaaaAAARK");
+
+    //var main =$("#main");
+
+    //main.on("mouseenter", function () {
+    //    main.style = "background-color: #888;";
+    //});
+
+    //main.on("mouseleave", function () {
+    //    main.style = "";
+    //});
+
+    //var menuitems = $("ul.menu li a");
+    //menuitems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+    
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+})();
